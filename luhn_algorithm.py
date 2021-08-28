f9eee4161569fc0c14db686ce381b58b2225ed01
AnirveshArcot <arcotanirvesh@gmail.com>
2021-08-28 23:13:30 +0530

    rework

--- luhn_algorithm.py
+++ luhn_algorithm.py
@@ -3,15 +3,15 @@ clear()
 choice=int(input("What would you like to do :\n 1.Check if a number follows the Luhn Algorithm (Enter 1 to choose this option )\n 2.Generate random numbers that follow the Luhn Algorithm (Enter 2 for this option)\n "))
 if choice == 1 :
     clear()
-    q1=input("Would you like to check if your credit card is valid (y/n) : ")
+    q1=input("Would you like to check if your number is valid (y/n) : ")
     if q1 == 'y':
       card = input("Enter your card no : ")
       f = open("NumLogs.txt", "a") 
       if(luhn_algorithm(card)):                 #The boolean value is returned here where its directly integrated with the if staement without the use of a variable 
-          print("The card number '"+card+"' is valid!")
+          print("The number '"+card+"' is valid!")
           f.write(card+"  1\n")
       else:
-          print("The card number '"+card+"' is NOT valid!")
+          print("The number '"+card+"' is NOT valid!")
           f.write(card+"  0\n")
     elif q1=='n':
        print("Okay then,keep your secrets.")
@@ -25,12 +25,14 @@ if choice == 1 :
 elif choice == 2 :
     name=platform.system()
     clear()
-    r=int(input("How many credit card numbers do you want?\n"))
+    r=int(input("How many numbers do you want?\n"))
     q=0
-    x=int(1000000000000000)
-    y=int(10000000000000000)
+    x=int(input("Enter how long your number must be"))
+    y=x+1
+    x2=pow(10 , x) 
+    y2=pow(10, x)
     while (q<r) :
-        card=random.randint(x,y)
+        card=random.randint(x2,y2)
         if (luhn_algorithm(str(card))):
             print(card_format(card))
             q=q+1
